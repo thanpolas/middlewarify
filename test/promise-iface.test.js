d2be1aedaba93dad1300c980c6d0073aa668e2dd
Thanasis Polychronakis <thanpolas@gmail.com>
2014-02-09 01:31:50 +0200

    write async resolution / rejection tests

--- test/promise-iface.test.js
+++ test/promise-iface.test.js
@@ -21,8 +21,8 @@ suite('7. Promise Interface', function() {
 
 
   function applyTests(num, middMethod, thing) {
+    var middOpts = {};
     setup(function() {
-      var middOpts = {};
 
       if (middMethod !== 'use') {
         middOpts = {beforeAfter: true};
@@ -78,6 +78,51 @@ suite('7. Promise Interface', function() {
 
       thing.create(1).then(done, done);
     });
+
+    test('7.' + num + '.5 async resolution', function(done) {
+      var invoked = false;
+      thing.create[middMethod](function() {
+        return new Promise(function(resolve) {
+          setTimeout(function(){
+            invoked = true;
+            resolve();
+          });
+        });
+      });
+      thing.create().then(function(){
+        assert.ok(invoked);
+      }).then(done, done);
+    });
+    test('7.' + num + '.6 async rejection', function(done) {
+      var invoked = false;
+      thing.create[middMethod](function() {
+        return new Promise(function(resolve, reject) {
+          setTimeout(function(){
+            invoked = true;
+            reject();
+          });
+        });
+      });
+      thing.create().then(null, function() {
+        assert.ok(invoked);
+      }).then(done, done);
+    });
+
+    test('7.' + num + '.7 async resolution for final callback', function(done) {
+      var invoked = false;
+      thing = Object.create(null);
+      midd.make(thing, 'create', function() {
+        return new Promise(function(resolve) {
+          setTimeout(function(){
+            invoked = true;
+            resolve();
+          });
+        });
+      }, middOpts);
+      thing.create().then(function() {
+        assert.ok(invoked);
+      }).then(done, done);
+    });
   }
 
   suite('7.1 Middleware with use()', function() {
@@ -114,5 +159,6 @@ suite('7. Promise Interface', function() {
         done();
       }).then(null, done);
     });
+
   });
 });
