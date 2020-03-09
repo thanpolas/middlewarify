63b9410422498a3d6c9fe11e80c6750914e6cda9
Thanasis Polychronakis <thanpolas@gmail.com>
2020-03-09 20:35:54 +0000

    one more test case for defaults

--- test/middlewarify.test.js
+++ test/middlewarify.test.js
@@ -7,19 +7,17 @@ const { assert } = require('chai');
 const midd = require('../');
 
 suite('1. Basic Tests', function() {
-  setup(function() {});
-
-  teardown(function() {});
-
-  // The numbering (e.g. 1.1.1) has nothing to do with order
-  // The purpose is to provide a unique string so specific tests are
-  // run by using the mocha --grep "1.1.1" option.
-
   test('1.1 Types Test', function() {
     const obj = Object.create(null);
     midd.make(obj, 'create');
     assert.isFunction(obj.create, 'obj.create should be a function');
     assert.isFunction(obj.create.use, 'obj.create.use should be a function');
+  });
+  test('1.2 Default return value', function() {
+    const obj = Object.create(null);
+    midd.make(obj, 'create');
+    const ret = obj.create();
+    assert.isUndefined(ret);
   });
 });
 
